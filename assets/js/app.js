f4225425d8a42c771d73f905625f1c9ec4643fcd
Ruslam Komlikov <komlikov.r@gmail.com>
2018-10-23 21:55:02 +0300

    add assets for front

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,16 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// any CSS you require will output into a single css file (app.scss in this case)
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// var $ = require('jquery');
+const $ = require('jquery');
+
+require('bootstrap');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
