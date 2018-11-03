ba78d7d86509ea82d0286ceaed9f5c163cea2e68
Ruslam Komlikov <komlikov.r@gmail.com>
2018-11-03 19:08:58 +0200

    add pagination

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CategoryController extends AbstractController
+class CategoryController extends Controller
 {
     /**
      * @Route("/category", name="category")
@@ -19,9 +21,28 @@ class CategoryController extends AbstractController
 
     /**
      * @Route("/category/{slug}", name="category_show")
+     * @param $slug
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function show()
+    public function show(Request $request, $slug)
     {
-
+        $category = $this->getDoctrine()->getRepository(Category::class)->findOneBy([
+            'slug' => $slug
+        ]);
+        $categoryId = $category->getId();
+        $em    = $this->getDoctrine()->getManager();
+        $dql   = "SELECT p FROM App\Entity\Product p WHERE p.category=$categoryId";
+        $query = $em->createQuery($dql);
+        $paginator  = $this->get('knp_paginator');
+        $products = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            $request->query->getInt('limit', 3)/*limit per page*/
+        );
+        return $this->render('/category/show.html.twig', [
+            'category' => $category,
+            'products' => $products
+        ]);
     }
 }
